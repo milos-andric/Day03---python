1f853cec85c99926194b43b2313197f93472f695
milos-andric <55629183+milos-andric@users.noreply.github.com>
2020-05-30 01:50:27 +0200

    norm

--- ex00/NumPyCreator.py
+++ ex00/NumPyCreator.py
@@ -1,21 +1,21 @@
 import numpy
 
+
 class NumPyCreator:
-	def from_list(lst):
-		return (np,array(lst))
-	
-	def from_tuple(lst):
-		return (np,array(lst))
+    def from_list(lst):
+        return (np, array(lst))
 
-	def from_iterable(lst):
-		
-		return(np,array(lst))
+    def from_tuple(lst):
+        return (np, array(lst))
 
-	def from_shape(shape):
-		return(np.zeros(shape))
+    def from_iterable(lst):
+        return(np, array(lst))
 
-	def random(shape):
-		return(np.random.rand(shape))
+    def from_shape(shape):
+        return(np.zeros(shape))
 
-	def identity(num)
-	return(np.identity(num))
+    def random(shape):
+        return(np.random.rand(shape))
+
+    def identity(num)
+    return(np.identity(num))
--- ex01/ImageProcessor.py
+++ ex01/ImageProcessor.py
@@ -2,15 +2,17 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
 
-class ImageProcessor:
-	def load(self, path):
-		img = mpimg.imread(path)
-		print("Loading image of dimensions " + str(np.shape(img)[0]) + " x " + str(np.shape(img)[1]))
-		return(img)
 
-	def display(self, array):
-		imgplot = plt.imshow(array)
-		plt.show()
+class ImageProcessor:
+    def load(self, path):
+        img = mpimg.imread(path)
+        print("Loading image of dimensions "
+              + str(np.shape(img)[0]) + " x " + str(np.shape(img)[1]))
+        return(img)
+
+    def display(self, array):
+        imgplot = plt.imshow(array)
+        plt.show()
 
 
 imp = ImageProcessor()
